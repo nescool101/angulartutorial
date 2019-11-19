15d68af41cfd7f2032e9139a07f82ac84a5c2017
nestor alvarez <nescool101@gmail.com>
2019-11-19 15:03:48 -0500

    week1day3

---
+++ whateverweek1day3.js
@@ -0,0 +1,228 @@
+
+<script type="text/javascript">
+<!--
+// This is a single line JavaScript comment
+
+document.write("I have comments in my JavaScript code!");
+//document.write("You can't see this!");
+//-->
+</script>
+<script type="text/javascript">
+<!--
+// This is a single line JavaScript comment
+
+document.write("I have comments in my JavaScript code!");
+//document.write("You can't see this!");
+//-->
+</script>
+<script type="text/javascript">
+<!--
+// This is a single line JavaScript comment
+
+document.write("I have comments in my JavaScript code!");
+//document.write("You can't see this!");
+//-->
+</script>
+<script type="text/javascript">
+<!--
+// This is a single line JavaScript comment
+
+document.write("I have comments in my JavaScript code!");
+//document.write("You can't see this!");
+//-->
+</script>
+<script type="text/javascript">
+<!--
+// This is a single line JavaScript comment
+
+document.write("I have comments in my JavaScript code!");
+//document.write("You can't see this!");
+//-->
+</script>
+<script type="text/javascript">
+<!--
+// This is a single line JavaScript comment
+
+document.write("I have comments in my JavaScript code!");
+//document.write("You can't see this!");
+//-->
+</script>
+<script type="text/javascript">
+<!--
+// This is a single line JavaScript comment
+
+document.write("I have comments in my JavaScript code!");
+//document.write("You can't see this!");
+//-->
+</script>
+
+<script type="text/javascript">
+<!--
+// This is a single line JavaScript comment
+
+document.write("I have comments in my JavaScript code!");
+//document.write("You can't see this!");
+//-->
+</script>
+<script type="text/javascript">
+<!--
+// This is a single line JavaScript comment
+
+document.write("I have comments in my JavaScript code!");
+//document.write("You can't see this!");
+//-->
+</script>
+<script type="text/javascript">
+<!--
+// This is a single line JavaScript comment
+
+document.write("I have comments in my JavaScript code!");
+//document.write("You can't see this!");
+//-->
+</script>
+<script type="text/javascript">
+<!--
+// This is a single line JavaScript comment
+
+document.write("I have comments in my JavaScript code!");
+//document.write("You can't see this!");
+//-->
+</script>
+<script type="text/javascript">
+<!--
+// This is a single line JavaScript comment
+
+document.write("I have comments in my JavaScript code!");
+//document.write("You can't see this!");
+//-->
+</script>
+<script type="text/javascript">
+<!--
+// This is a single line JavaScript comment
+
+document.write("I have comments in my JavaScript code!");
+//document.write("You can't see this!");
+//-->
+</script>
+<script type="text/javascript">
+<!--
+// This is a single line JavaScript comment
+
+document.write("I have comments in my JavaScript code!");
+//document.write("You can't see this!");
+//-->
+</script>
+
+<script type="text/javascript">
+<!--
+// This is a single line JavaScript comment
+
+document.write("I have comments in my JavaScript code!");
+//document.write("You can't see this!");
+//-->
+</script>
+<script type="text/javascript">
+<!--
+// This is a single line JavaScript comment
+
+document.write("I have comments in my JavaScript code!");
+//document.write("You can't see this!");
+//-->
+</script>
+<script type="text/javascript">
+<!--
+// This is a single line JavaScript comment
+
+document.write("I have comments in my JavaScript code!");
+//document.write("You can't see this!");
+//-->
+</script>
+<script type="text/javascript">
+<!--
+// This is a single line JavaScript comment
+
+document.write("I have comments in my JavaScript code!");
+//document.write("You can't see this!");
+//-->
+</script>
+<script type="text/javascript">
+<!--
+// This is a single line JavaScript comment
+
+document.write("I have comments in my JavaScript code!");
+//document.write("You can't see this!");
+//-->
+</script>
+<script type="text/javascript">
+<!--
+// This is a single line JavaScript comment
+
+document.write("I have comments in my JavaScript code!");
+//document.write("You can't see this!");
+//-->
+</script>
+<script type="text/javascript">
+<!--
+// This is a single line JavaScript comment
+
+document.write("I have comments in my JavaScript code!");
+//document.write("You can't see this!");
+//-->
+</script>
+
+<script type="text/javascript">
+<!--
+// This is a single line JavaScript comment
+
+document.write("I have comments in my JavaScript code!");
+//document.write("You can't see this!");
+//-->
+</script>
+<script type="text/javascript">
+<!--
+// This is a single line JavaScript comment
+
+document.write("I have comments in my JavaScript code!");
+//document.write("You can't see this!");
+//-->
+</script>
+<script type="text/javascript">
+<!--
+// This is a single line JavaScript comment
+
+document.write("I have comments in my JavaScript code!");
+//document.write("You can't see this!");
+//-->
+</script>
+<script type="text/javascript">
+<!--
+// This is a single line JavaScript comment
+
+document.write("I have comments in my JavaScript code!");
+//document.write("You can't see this!");
+//-->
+</script>
+<script type="text/javascript">
+<!--
+// This is a single line JavaScript comment
+
+document.write("I have comments in my JavaScript code!");
+//document.write("You can't see this!");
+//-->
+</script>
+<script type="text/javascript">
+<!--
+// This is a single line JavaScript comment
+
+document.write("I have comments in my JavaScript code!");
+//document.write("You can't see this!");
+//-->
+</script>
+<script type="text/javascript">
+<!--
+// This is a single line JavaScript comment
+
+document.write("I have comments in my JavaScript code!");
+//document.write("You can't see this!");
+//-->
+</script>
